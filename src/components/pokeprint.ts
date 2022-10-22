9ff3930cecd945946edcfe615b6553cd09a5215f
Rafael <grafa74@gmail.com>
2022-10-22 21:32:13 +0200

    got some button action going

--- src/components/pokeprint.ts
+++ src/components/pokeprint.ts
@@ -6,17 +6,19 @@ export class PokePrint extends Component {
     pokes: any;
     pokesInfo: any;
     api: PokeApi;
+    nextPageInfo: any;
+    nextPagePokes: any;
     constructor(public selector: string) {
         super();
         this.api = new PokeApi();
         this.pokes = [];
-        this.pokesInfo = '';
+        this.pokesInfo = [];
         this.startFetch();
     }
 
     async startFetch() {
         this.pokes = await this.api.getPoke();
-        console.log(this.pokes);
+        //console.log(this.pokes);
 
         const pokemonArr: any = [];
         this.pokes.results.forEach((item: any) => {
@@ -26,12 +28,30 @@ export class PokePrint extends Component {
             pokemonArr.map((url: any) => fetch(url).then((r) => r.json()))
         );
         //console.log(this.pokesInfo);
+        this.nextPageInfo = await this.api.getNextPage(this.pokes.next);
+        const nextPokeArray: any = [];
+
+        this.nextPageInfo.results.forEach((item: any) => {
+            nextPokeArray.push(item.url);
+        });
+        this.nextPagePokes = await Promise.all(
+            nextPokeArray.map((url: any) =>
+                fetch(url).then((result) => result.json())
+            )
+        );
+
         this.manageComponent();
     }
 
     manageComponent() {
-        this.template = this.createTemplate();
+        this.template = this.createTemplate(this.pokesInfo);
         this.renderAdd(this.selector, this.template);
+
+        document.querySelector('.btn-next')?.addEventListener('click', () => {
+            console.log(this.nextPagePokes);
+            this.template = this.createTemplate(this.nextPagePokes);
+            this.render(this.selector, this.template);
+        });
     }
 
     createTemplate() {
@@ -42,6 +62,18 @@ export class PokePrint extends Component {
             this.template += `<img class="pokes-img" src="${pokemon.sprites.front_default}" alt="" width="100">
        </div>`;
         });
+
+        this.template += `</div>
+         <div class="buttons-container">
+        <button class="btn-previous">
+         <a href=''>Atras</a>
+        </button>
+                          
+        <button class="btn-next">
+     
+          Siguiente
+         </button>
+         </div>`;
 
         return this.template;
     }
--- src/services/poke.api.ts
+++ src/services/poke.api.ts
@@ -8,52 +8,7 @@ export class PokeApi {
     getPoke(): Promise<Array<PokeApi>> {
         return fetch(this.urlDefault).then((response) => response.json());
     }
-}
-
-export class PokePages {
-    urlPagination: string;
-    constructor() {
-        this.urlPagination =
-            'https://pokeapi.co/api/v2/pokemon?offset=20&limit=20';
-    }
-
-    getPokePage(): Promise<Array<PokePages>> {
-        return fetch(this.urlPagination).then((response) => response.json());
+    getNextPage(nextUrl: string): Promise<any> {
+        return fetch(nextUrl).then((response) => response.json());
     }
 }
-/*
-
-
-
-
-fetch(‘https://pokeapi.co/api/v2/pokemon?limit=151')  .then(response => response.json())  .then(allpokemon => console.log(allpokemon))
-    // create / post
-    createTask(task: IPokemon): Promise<PokeApi> {
-        return fetch(this.url, {
-            method: 'POST',
-            body: JSON.stringify(task),
-            headers: {
-                'content-type': 'application/json',
-            },
-        }).then((response) => response.json());
-    }
-
-    // delete
-    deleteTask(id: number): Promise<Response> {
-        return fetch(`${this.url}/${id}`, {
-            method: 'DELETE',
-        });
-    }
-
-    // uptate / patch
-    updateTask(id: number, partialTask: Partial<IPokemon>): Promise<Task> {
-        return fetch(`${this.url}/${id}`, {
-            method: 'PATCH',
-            body: JSON.stringify(partialTask),
-            headers: {
-                'content-type': 'application/json',
-            },
-        }).then((response) => response.json());
-    }
-
-*/
